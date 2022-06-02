36e01283852afb5bbf2d2d02245861e688a9d544
Cosmosec <151138141+Cosmosec@users.noreply.github.com>
2022-06-02 17:35:46 +0000

    adding a user subscription

--- pick_place.cpp
+++ pick_place.cpp
@@ -161,4 +161,12 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface& pla
   collision_objects[0].id = "table1";
   collision_objects[0].header.frame_id = "base_link";
 
-  /* Define the primitive and its dimens
+  /* Define the primitive and its dimensions. */
+  collision_objects[0].primitives.resize(1);
+  collision_objects[0].primitives[0].type = collision_objects[0].primitives[0].BOX;
+  collision_objects[0].primitives[0].dimensions.resize(3);
+  collision_objects[0].primitives[0].dimensions[0] = 1.0;
+  collision_objects[0].primitives[0].dimensions[1] = 1.0;
+  collision_objects[0].primitives[0].dimensions[2] = 0.47;
+
+  /* Define the pose of the
